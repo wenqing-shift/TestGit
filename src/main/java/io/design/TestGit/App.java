84863afbdd3f5f5eddd4f5546c15ff70530f5aae
eclipse_user <eclipse_user@itree.com>
2020-03-02 21:26:53 +0800

    my first commit in eclipse

---
+++ src/main/java/io/design/TestGit/App.java
@@ -0,0 +1,13 @@
+package io.design.TestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
